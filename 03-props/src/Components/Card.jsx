abe1e94b1ebee653ad5cfa767603e5b970d6f954
mtmsujon <mtmsujon@gmail.com>
2025-10-28 17:49:11 +0600

    update card props function

--- 03-props/src/Components/Card.jsx
+++ 03-props/src/Components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = ({name, designation, website, image}) => {
+const Card = (props) => {
     return(
         <div className="card">
             <img
-            src={image}
-            alt={name}
+            src={props.image}
+            alt={props.name}
             />
-            <h1>{name}</h1>
-            <p>{designation}</p>
-            <p>{website}</p>
+            <h1>{props.name}</h1>
+            <p>{props.designation}</p>
+            <p>{props.website}</p>
             <button>Follow</button>
         </div>
     );
